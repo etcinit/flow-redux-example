04b7b8833a1626c7e841fcbc9a96f9b055230b96
Eduardo Trujillo <ed@chromabits.com>
2016-12-26 11:44:28 -0800

    test(ToDoList): Add remaining tests

--- src/components/ToDoList.test.js
+++ src/components/ToDoList.test.js
@@ -37,3 +37,77 @@ it('should show a message when there are no tasks and hidden done', () => {
   expect(wrapper.find('small').text())
     .toEqual('Add an item to begin. There are 2 completed tasks.');
 });
+
+it('should only render pending tasks if done are hidden', () => {
+  const doneItem: Item = {
+    content: 'some content',
+    createdAt: moment(),
+    toggledAt: moment(),
+    done: true,
+    editing: false,
+  };
+  const pendingItem: Item = {
+    ...doneItem,
+    done: false,
+  };
+
+  const items: Map<string, Item> = Map()
+    .set('firstItem', doneItem)
+    .set('secondItem', pendingItem);
+
+  const wrapper = shallow(
+    <ToDoList items={ items } settings={ { doneVisible: false } }/>
+  );
+
+  expect(wrapper.find({ item: pendingItem }).exists()).toBeTruthy();
+});
+
+it('should render all tasks if done are visible', () => {
+  const doneItem: Item = {
+    content: 'some content',
+    createdAt: moment(),
+    toggledAt: moment(),
+    done: true,
+    editing: false,
+  };
+  const pendingItem: Item = {
+    ...doneItem,
+    done: false,
+  };
+
+  const items: Map<string, Item> = Map()
+    .set('firstItem', doneItem)
+    .set('secondItem', pendingItem);
+
+  const wrapper = shallow(
+    <ToDoList items={ items } settings={ { doneVisible: true } }/>
+  );
+
+  expect(wrapper.find({ item: pendingItem }).exists()).toBeTruthy();
+  expect(wrapper.find({ item: doneItem }).exists()).toBeTruthy();
+});
+
+it('should still render tasks when all are done', () => {
+  const doneItem: Item = {
+    content: 'some content',
+    createdAt: moment(),
+    toggledAt: moment(),
+    done: true,
+    editing: false,
+  };
+  const doneItem2: Item = {
+    ...doneItem,
+    content: 'some other content',
+  };
+
+  const items: Map<string, Item> = Map()
+    .set('firstItem', doneItem)
+    .set('secondItem', doneItem2);
+
+  const wrapper = shallow(
+    <ToDoList items={ items } settings={ { doneVisible: true } }/>
+  );
+
+  expect(wrapper.find({ item: doneItem }).exists()).toBeTruthy();
+  expect(wrapper.find({ item: doneItem2 }).exists()).toBeTruthy();
+});
